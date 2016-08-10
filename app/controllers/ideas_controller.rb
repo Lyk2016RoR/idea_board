5e8c1203057a0fdb6e6d66856adeb82df49187a2
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2016-08-10 21:16:11 +0300

    Add new idea form

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,17 +1,27 @@
 class IdeasController < ApplicationController
 	def new
-		@name = "Saygin"
+		@idea = Idea.new
 	end
 
 	def index
 		@ideas = Idea.all
 	end
 
-
-
-
-	
 	def show
 		@idea = Idea.find(params[:id])
+	end
+
+	def create
+		@idea = Idea.new(strong_params)
+
+		if @idea.save
+			redirect_to idea_path(@idea)
+		else
+			render :new
+		end
+	end
+
+	def strong_params
+		params.permit(:title, :description, :planned_to)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root "ideas#index"
   get 'ideas/new', to: 'ideas#new'
   get 'ideas', to: 'ideas#index'
-  get 'ideas/:id', to: 'ideas#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'ideas/:id', to: 'ideas#show', as: "idea"
+  post 'ideas', to: 'ideas#create'
 end
