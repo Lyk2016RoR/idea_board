34656b9af6d702962a93d319c2c9a21d8a845f28
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2016-08-10 10:49:50 +0300

    Add idea display page

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -6,4 +6,12 @@ class IdeasController < ApplicationController
 	def index
 		@ideas = Idea.all
 	end
+
+
+
+
+	
+	def show
+		@idea = Idea.find(params[:id])
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root "ideas#index"
   get 'ideas/new', to: 'ideas#new'
   get 'ideas', to: 'ideas#index'
+  get 'ideas/:id', to: 'ideas#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
