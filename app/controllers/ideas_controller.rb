1f1aa8d925b7d5d1f7b12dcd49a73d8103e52654
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2016-08-12 15:30:55 +0300

    Add delete link

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -35,6 +35,12 @@ class IdeasController < ApplicationController
 		end
 	end
 
+	def destroy
+		@idea = Idea.find(params[:id])
+		@idea.destroy
+		redirect_to ideas_path
+	end
+
 	def idea_params
 		params.permit(:title, :description, :planned_to)
 	end
