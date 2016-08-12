6b11712d6b9ed25e7982d459bc4212af741f2a16
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2016-08-12 14:38:54 +0300

    Update links to use link_to helper

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'ideas', to: 'ideas#index'
   get 'ideas/:id', to: 'ideas#show', as: "idea"
   post 'ideas', to: 'ideas#create'
-  get 'ideas/:id/edit', to: 'ideas#edit'
+  get 'ideas/:id/edit', to: 'ideas#edit', as: "edit_idea"
   put 'ideas/:id', to: 'ideas#update'
 end
