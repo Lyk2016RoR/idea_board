1cac39d4cdeae24b182e89c0e865b10fe80f8924
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2016-08-13 15:06:16 +0300

    Add CategoriesController index and show actions

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
   validates :color, inclusion: {in: %w(red blue green),
     message: "kırmızı mavi yeşil olabilir"}
   validates :title, uniqueness: true
+
+  def ideas
+    Idea.where(category_id: id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
   root "ideas#index"
   resources :ideas
+  resources :categories, only: [:show, :index]
 end
