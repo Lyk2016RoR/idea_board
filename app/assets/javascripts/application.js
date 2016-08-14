485c2abc0f58cd38917f790acaeb0d277f9416f4
MuhammetDilmac <iletisim@muhammetdilmac.com.tr>
2016-08-14 21:28:46 +0300

    Tasarım giydirilmeye başlandı

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,27 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap.min
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+
+$(document).ready( function(){
+  window.onresize = function(event) {
+    if( $(document).width() > 1200 ) fixedNavbar();
+  };
+
+  if( $(document).width() > 1200 ) fixedNavbar();
+});
+
+function fixedNavbar() {
+  var navbar_right = $('#navbar-right'),
+      navbar_top = $('#navbar-top'),
+      caurusel = $('#myCarousel'),
+      total_height = navbar_top.height() + caurusel.height();
+
+  navbar_right.affix({
+      offset: {
+        top: total_height
+      }
+  });
+}
