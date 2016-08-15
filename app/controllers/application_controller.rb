ef1e709dd18018ce26ce8fd9e6e1ae1eb419bf84
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2016-08-15 10:24:45 +0300

    Fix logo link, add displaying category list in sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :load_sidebar_categories
+
+  def load_sidebar_categories
+    @sidebar_categories = Category.all
+  end
 end
