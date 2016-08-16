df1fc153f8ff0dd3557d82a824e3367756ae8f1f
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2016-08-16 11:10:35 +0300

    Add idea assign to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :ideas, dependent: :destroy
+  has_many :ideas
   has_many :comments, dependent: :destroy
+
+  before_destroy :assign_to_first_user
+
+  private
+
+    def assign_to_first_user
+        ideas.update_all(user_id: User.first.id)
+    end
+
 end
