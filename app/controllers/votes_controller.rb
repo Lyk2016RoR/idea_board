5cf9b90784e1a56f7054ee02c75dd6cb5fdfe2b9
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2016-08-16 12:15:23 +0300

    Implement votes#create

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,7 +3,15 @@ class VotesController < ApplicationController
   before_action :set_idea
 
   def create
-    #@vote = @idea.votes.new(rating: params[:vote][:rating])
+    @vote = @idea.votes.new
+    @vote.rating = params[:vote][:rating]
+    @vote.user = current_user
+
+    if @vote.save
+      redirect_to @idea, notice: "Vote was saved."
+    else
+      redirect_to @idea, notice: "Vote is not valid."
+    end
   end
 
   def destroy
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :idea
+
+  validates :rating, inclusion: (1..5)
+  validates :user_id, uniqueness: {scope: :idea_id}
 end
