23fc769b8c9d7c3868bd68aad49d6caf3a3f3a81
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2016-08-16 11:22:24 +0300

    Fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Creating seed data..."
 
+u = User.create(email: "admin@admin.com", password: 123456)
+
 c1 = Category.create(title: "Genel", desc: 'Genel kategorimiz burasıdır.', color: 'red')
 c2 = Category.create(title: "RoR", desc: 'Ruby on Rails ile ilgili fikirlerin bulunduğu kategorimiz.', color: 'green')
 
-Idea.create(title: 'Züper Fikir', description: 'Über Züper Fikir', category: c1)
-Idea.create(title: 'Süper Fikir!', description: 'Bence bilişim terimleri tekrar düzenlensin.', category_id: c1.id)
-Idea.create(title: 'Deneme Fikir', description: 'Test descriptionım.', category: c2)
+Idea.create(title: 'Züper Fikir', description: 'Über Züper Fikir', category: c1, user: u)
+Idea.create(title: 'Süper Fikir!', description: 'Bence bilişim terimleri tekrar düzenlensin.', category_id: c1.id, user: u)
+Idea.create(title: 'Deneme Fikir', description: 'Test descriptionım.', category: c2, user: u)
 
 puts "Seed data created"
