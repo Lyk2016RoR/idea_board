f4e17af00d0bb8c434d7e5ab511171ddb035fcec
Umurcan Görür <mail@ugorur.com>
2016-08-17 11:50:29 +0300

    add tag links to application.html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :load_sidebar_categories
+  before_action :load_sidebar_datas
 
-  def load_sidebar_categories
+  def load_sidebar_datas
     @sidebar_categories = Category.all
+    @sidebar_tags = Tag.all
   end
 end
